a3a8204349d88719b86041da3bc556d658f5dfda
fuwenhao <fuwenhao@acoinfo.com>
2023-04-11 12:16:28 +0800

    feat:base

---
+++ README.md
@@ -0,0 +1,6 @@
+# VSOA 应用开发模板
+- 内置 VSOA Mock Server
+
+## 应用包含功能
+- vsoa client  相关功能演示
+- vsoa server  相关功能演示
---
+++ routers/index.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020 EdgerOS Team.
+ * All rights reserved.
+ *
+ * Detailed license information can be found in the LICENSE file.
+ *
+ * File: demo rest api.
+ *
+ * Author: Li.Ping <liping@acoinfo.com>
+ *
+ */
+const WebApp = require('webapp');
+const Router = WebApp.Router;
+const router = Router.create();
+
+/* Test call */
+router.get('/test', function(req, res) {
+	res.send('Hello world!');
+});
+
+
+module.exports = router
