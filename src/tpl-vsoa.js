449f4bc9b9598fa5b0587e930a865e5ede419724
fuwenhao <fuwenhao@acoinfo.com>
2023-04-17 11:52:59 +0800

    feat:socket.io client

--- src/tpl-vsoa.js
+++ src/tpl-vsoa.js
@@ -11,6 +11,7 @@
 
 /* Import system modules */
 const WebApp = require('webapp');
+const socketIo = require('socket.io')
 const router = require('./router');
 const vsoaCliSer = require('./vsoa-client-service');
 
@@ -25,6 +26,26 @@ app.use(WebApp.static('./public'));
 /* Set test rest */
 app.use('/api', router);
 
+const io = socketIo(app, {
+    path: '/socket.io',
+    serveClient: false,
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+});
+
+io.on('connect',(socket)=>{
+    
+    console.log('connect socket.io!',socket.id)
+
+    socket.on('auth',(msg)=>{
+        console.log('auth msg socket.io!',msg)
+    })
+    
+    socket.on('message',(msg)=>{
+        console.log('auth msg socket.io!',msg)
+    })
+})
 
 
 /* 当vsoa server 启动后启动 EAP后台  */
@@ -32,7 +53,7 @@ new Task('src/vsoa-server-task.js');
 Task.on('message', function (msg, from) {
     if (msg.type === 'vsoa server') {
         /* 初始化 vsoa client 实例 */
-        vsoaCliSer.vsoaClientInit({port: msg.port})
+        vsoaCliSer.vsoaClientInit({ port: msg.port })
 
         /* Start App */
         app.start();
